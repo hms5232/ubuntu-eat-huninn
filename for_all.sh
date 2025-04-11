7b6308824757064f1099515afc87c443ad5cedf3
hms5232 <43672033+hms5232@users.noreply.github.com>
2025-04-11 19:30:40 +0800

    👽️ 因應粉圓 release 改為上傳 ttf 變更流程

--- for_all.sh
+++ for_all.sh
@@ -14,27 +14,43 @@ if [ ! -d "$HOME/huninn" ] ; then
 fi
 
 url=$(curl -s https://api.github.com/repos/justfont/open-huninn-font/releases/latest | grep "browser_download_url" | awk '{ print $2 }' | sed 's/"//g')
-zipname=$(basename $url .zip)
+extension="${url##*.}"
 
-echo "Now download $zipname from $url"
+if [ "$extension" = "zip" ]; then
+	zipname=$(basename $url .zip)
 
-# Let's download font
-# download 粉圓
-wget -O huninn.zip $url
+	echo "Now download $zipname from $url"
 
-# unzip
-unzip huninn.zip
+	# Let's download font
+	# download 粉圓
+	wget -O huninn.zip $url
 
-# check zip file download and upzip succefully
-if [ ! -d "$zipname" ] ; then
+	# unzip
+	unzip huninn.zip
+
+	# check zip file download and upzip succefully
+	if [ ! -d "$zipname" ] ; then
+		echo -e "\n\n"
+		echo -e "\a========= The font file went wrong! Please try again later. =========\n"
+		exit 1
+	fi
+
+	# copy fonts to ~/huninn
 	echo -e "\n\n"
-	echo -e "\a========= The font file went wrong! Please try again later. =========\n"
-	exit 1
+	cp -i $zipname/*.ttf $HOME/huninn
+
+	# remove zip and unzip dir
+	rm huninn.zip
+	rm -r $zipname
+else
+	filename=$(basename $url)
+    echo "Now download $filename from $url"
+    wget $url
+    # copy fonts to ~/huninn
+    echo -e "\n\n"
+    mv *.ttf $HOME/huninn
 fi
 
-# copy fonts to ~/.fonts
-echo -e "\n\n"
-cp -i $zipname/*.ttf $HOME/huninn
 # move folder to system fonts folder
 sudo mv -i $HOME/huninn /usr/local/share/fonts/
 # change permission
@@ -45,10 +61,6 @@ sudo chmod 755 /usr/local/share/fonts/huninn
 # refresh fonts cache
 echo -e "\n\n"
 sudo fc-cache -fv
-
-# remove zip and unzip dir
-rm huninn.zip
-rm -r $zipname
 
 # finally
 echo -e "\n\n"
--- for_me.sh
+++ for_me.sh
@@ -14,35 +14,46 @@ if [ ! -d "$HOME/.fonts" ] ; then
 fi
 
 url=$(curl -s https://api.github.com/repos/justfont/open-huninn-font/releases/latest | grep "browser_download_url" | awk '{ print $2 }' | sed 's/"//g')
-zipname=$(basename $url .zip)
+extension="${url##*.}"
 
-echo "Now download $zipname from $url"
-
-# Let's download font
-# download 粉圓
-wget -O huninn.zip $url
-
-# unzip
-unzip huninn.zip
-
-# check zip file download and upzip succefully
-if [ ! -d "$zipname" ] ; then
-	echo -e "\n\n"
-	echo -e "\a========= The font file went wrong! Please try again later. =========\n"
-	exit 1
+if [ "$extension" = "zip" ]; then
+    zipname=$(basename $url .zip)
+    
+    echo "Now download $zipname from $url"
+    
+    # Let's download font
+    # download 粉圓
+    wget -O huninn.zip $url
+    
+    # unzip
+    unzip huninn.zip
+    
+    # check zip file download and upzip succefully
+    if [ ! -d "$zipname" ] ; then
+        echo -e "\n\n"
+        echo -e "\a========= The font file went wrong! Please try again later. =========\n"
+        exit 1
+    fi
+    
+    # copy fonts to ~/.fonts
+    echo -e "\n\n"
+    cp -i $zipname/*.ttf $HOME/.fonts
+    
+    # remove zip and unzip dir
+    rm huninn.zip
+    rm -r $zipname
+else
+    filename=$(basename $url)
+    echo "Now download $filename from $url"
+    wget $url
+    # copy fonts to ~/.fonts
+    echo -e "\n\n"
+    mv *.ttf $HOME/.fonts
 fi
-
-# copy fonts to ~/.fonts
-echo -e "\n\n"
-cp -i $zipname/*.ttf $HOME/.fonts
 
 # refresh fonts cache
 echo -e "\n"
 sudo fc-cache -fv
-
-# remove zip and unzip dir
-rm huninn.zip
-rm -r $zipname
 
 # finally
 echo -e "\n\n"
